96ffd9635d9eba3be745090e33f461dd8e6845f0
Juan Almeida <juan.satyro@gmail.com>
2017-11-19 16:05:06 -0200

    Componentes basicos do WebPack e React

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './ex/index.js',
+  entry: './app/index.jsx',
   output: {
     path: __dirname + '/public',
     filename: './bundle.js'
   },
-  devserver: {
-
+  devServer: {
+    port: 8000,
+    contentBase: './public'
+  },
+  plugins: [
+    new ExtractTextPlugin('app.css')
+  ],
+  resolve: {
+    extensions: ["*",'.js','.jsx']
+  },
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['es2015','react'],
+        plugins: ['transform-object-rest-spread']
+      }
+    },
+    {
+      test: /\. css$/,
+      loader: ExtractTextPlugin.extract("style-loader","css-loader")
+    }]
   }
 }
